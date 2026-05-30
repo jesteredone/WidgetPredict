51ee50e48dd86e2a0877414267f2eb7967fcd0c3
jesteredone <jesteredone@gmail.com>
2026-05-30 02:00:26 +0200

    app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const miCanal = "creador"; 
+const miCanal = "TU_USUARIO_DE_TWITCH"; 
 
 const contenedor = document.getElementById("contenedor-apuesta");
 const barraSi = document.getElementById("progreso-si");
@@ -7,37 +7,34 @@ const textoSi = document.getElementById("porcentaje-si");
 const textoNo = document.getElementById("porcentaje-no");
 const puntosSiCont = document.getElementById("puntos-si");
 const puntosNoCont = document.getElementById("puntos-no");
-const bandoSiPuntos = document.getElementById("bando-si-puntos");
-const bandoNoPuntos = document.getElementById("bando-no-puntos");
-const timerHtml = document.getElementById("tiempo");
-const labelOpcionSi = document.getElementById("texto-opcion-si");
-const labelOpcionNo = document.getElementById("texto-opcion-no");
 
 function actualizarDuelo(puntosSi, puntosNo) {
     const total = puntosSi + puntosNo;
     let porcSi = 50, porcNo = 50;
+    
     if (total > 0) {
         porcSi = Math.round((puntosSi / total) * 100);
         porcNo = 100 - porcSi;
     }
+    
     barraSi.style.width = porcSi + "%";
     barraNo.style.width = porcNo + "%";
+    
     textoSi.innerText = porcSi + "%";
     textoNo.innerText = porcNo + "%";
+    
     puntosSiCont.innerText = puntosSi.toLocaleString() + " PTS";
     puntosNoCont.innerText = puntosNo.toLocaleString() + " PTS";
 }
 
-function limpiarResultados() {
-    [barraSi, barraNo, bandoSiPuntos, bandoNoPuntos].forEach(el => el.classList.remove("ganador", "perdedor"));
+// Inicializar ComfyJS
+if(miCanal !== "TU_USUARIO_DE_TWITCH") {
+    ComfyJS.Init(miCanal);
 }
 
-// Lógica de Twitch (Solo se activará en el OBS de "creador")
-ComfyJS.Init(miCanal);
-
-// (Opcional: Si quieres probarlo tú, toca la pantalla)
+// Simulación de prueba al hacer clic en el widget dentro de OBS
 document.body.addEventListener("click", () => {
-    limpiarResultados();
-    contenedor.className = "mostrar";
-    actualizarDuelo(5000, 2000);
+    contenedor.classList.toggle("mostrar");
+    contenedor.classList.toggle("oculto");
+    actualizarDuelo(Math.floor(Math.random() * 10000), Math.floor(Math.random() * 10000));
 });
